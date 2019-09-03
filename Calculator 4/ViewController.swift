9d83f03b6c9f8d06fc317be4e3c253f3827cb440
Serena Finley <serena.grace.finley@gmail.com>
2019-09-03 14:02:53 -0500

    Calculator

--- Calculator 4/ViewController.swift
+++ Calculator 4/ViewController.swift
@@ -15,6 +15,40 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var solutionBox: UILabel!
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        let firstText = Int(firstTextField.text!)
+        let secondText = Int(secondTextField.text!)
+        let solutionVariable = firstText! + secondText!
+        solutionBox.text = String(solutionVariable)
+    }
+    
+    @IBAction func subtractButtonPressed(_ sender: Any) {
+        let firstText = Int(firstTextField.text!)
+        let secondText = Int(secondTextField.text!)
+        let solutionVariable = firstText! - secondText!
+        solutionBox.text = String(solutionVariable)
+    }
+    
+    @IBAction func divideButtonPressed(_ sender: Any) {
+        let firstText = Int(firstTextField.text!)
+        let secondText = Int(secondTextField.text!)
+        let solutionVariable = firstText! / secondText!
+        solutionBox.text = String(solutionVariable)    }
+    
+    @IBAction func multiplyButtonPressed(_ sender: Any) {
+        let firstText = Int(firstTextField.text!)
+        let secondText = Int(secondTextField.text!)
+        let solutionVariable = firstText! * secondText!
+        solutionBox.text = String(solutionVariable)
+    }
 
 }
+
+
+
+
 
